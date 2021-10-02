b40f53f61c1b7ae2ea04634b58b9fcb1267cc417
tanishabisht <agamyatani@gmail.com>
2021-10-02 16:53:20 +0530

    Networks : GET CREATE DELETE

--- src/Components/NetworkCard/NetworkCard.js
+++ src/Components/NetworkCard/NetworkCard.js
@@ -1,11 +1,19 @@
 import classes from './NetworkCard.module.css'
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from '../../Config/firebase'
 
 
 const NetworkCard = ({name}) => {
+
+    const deleteHandler = async() => {
+        await deleteDoc(doc(db, "Networks", name))
+    }
+
+    
     return (
         <div className={classes.Container}>
             <p>{name}</p>
-            <span>34</span>
+            <span onClick={deleteHandler}><i class="fa fa-trash-alt"></i></span>
         </div>
     );
 }
--- src/Pages/AllNetworks/AllNetworks.js
+++ src/Pages/AllNetworks/AllNetworks.js
@@ -1,39 +1,34 @@
 import { useState, useEffect } from 'react'
 import { NetworkCard } from '../../Components'
 import classes from './AllNetworks.module.css'
-
-import { doc, onSnapshot, collection } from 'firebase/firestore'
+import { doc, onSnapshot, collection, setDoc } from 'firebase/firestore'
 import { db } from '../../Config/firebase'
+
 
 
 const AllNetworks = () => {
 
-    const funct = () => {
-        const unsub = onSnapshot(collection(db, "network"), (snap) => {
-            console.log(snap.docs.map(doc => doc.data()));
+    const [allNetworks, setAllNetworks] = useState([])
+    const [newNetwork, setNewNetwork] = useState('')
+    const [searchString, setSearchString] = useState('')
+
+
+    const getRealtimeData = () => {
+        const unsub = onSnapshot(collection(db, "Networks"), (snap) => {
+            console.log(snap.docs.map(doc => doc.id));
+            setAllNetworks(snap.docs.map(doc => doc.id))
+            // console.log(snap.docs.map(doc => ({id: doc.id, ...doc.data()})));
         })
         return () => unsub()
     }
 
 
-    useEffect(funct, [])
-    
+    const addNetworkHandler = async() => {
+        await setDoc(doc(db, "Networks", newNetwork), {});
+    }
 
-    const allNetworksVar = [
-        'FINE NAMEE 1',
-        'NETWORK NAME 2',
-        'NETWORK FIND 3',
-        'NETWORK NAME 4',
-        'NETWORK NAMEE 5',
-        'NETWORK NAME 6',
-        'NETWORK NAMEE 7',
-        'NETWORK NAME 8',
-        'NETWORK NAME 9'
-    ]
 
-    const [allNetworks, setAllNetworks] = useState(allNetworksVar)
-    const [newNetwork, setNewNetwork] = useState(allNetworksVar)
-    const [searchString, setSearchString] = useState('')
+    useEffect(getRealtimeData, [])  
 
     const onSearchChange = e => setSearchString(e.target.value.trim().toLowerCase())
     const onNetworkChange = e => setNewNetwork(e.target.value)
@@ -43,7 +38,7 @@ const AllNetworks = () => {
 
             {/* ADD NETWORK */}
             <input type="text" name="network_name" className={classes.AddInput} onChange={onNetworkChange} placeholder='Add a new network' />
-            <button className={classes.AddBtn}>ADD</button>
+            <button onClick={addNetworkHandler} className={classes.AddBtn}>ADD</button>
             
             {/* SEARCH NETWORK */}
             <input type="text" name="network_name" className={classes.Input} onChange={onSearchChange} placeholder='Name of the network you want to find' />
@@ -58,3 +53,19 @@ const AllNetworks = () => {
 }
 
 export default AllNetworks;
+
+
+
+
+
+// const allNetworksVar = [
+//     'FINE NAMEE 1',
+//     'NETWORK NAME 2',
+//     'NETWORK FIND 3',
+//     'NETWORK NAME 4',
+//     'NETWORK NAMEE 5',
+//     'NETWORK NAME 6',
+//     'NETWORK NAMEE 7',
+//     'NETWORK NAME 8',
+//     'NETWORK NAME 9'
+// ]
