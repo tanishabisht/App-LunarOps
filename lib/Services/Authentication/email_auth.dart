e84e37a4126b54bb6da573f7c1e35bfd79a50142
PrakharKaushik213 <prakharkaushik213@gmail.com>
2021-10-02 13:22:46 +0530

    function added

--- lib/Services/Authentication/email_auth.dart
+++ lib/Services/Authentication/email_auth.dart
@@ -1,10 +1,25 @@
-import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class email_auth {
+class EmailAuth {
   String? _token;
   DateTime? _expiryDate;
   String? _userId;
-  Timer? expireTime;
+
+  Future<void> signup(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDttjNKuUs-R0bFhTrBF6Vp6udiXYpS8Dc');
+    final response = await http.post(url,
+        body: json.encode(
+            {'email': email, 'password': password, 'returnSecureToken': true}));
+    print(json.decode(response.body));
+    var res = json.decode(response.body);
+    if (res['error'] != null) {
+      throw Exception(res['error']);
+    }
+    _token = res['idToken'];
+    _userId = res['localId'];
+  }
 }
