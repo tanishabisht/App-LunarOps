16dd852b6df97008fdf2d91912ecf74cd3549796
PrakharKaushik213 <prakharkaushik213@gmail.com>
2021-10-03 12:27:20 +0530

    Completed

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -4,15 +4,19 @@ import 'package:lunarops/Services/Authentication/Email_Auth.dart';
 import 'package:video_player/video_player.dart';
 
 class SignupOptionScreen extends StatefulWidget {
-  const SignupOptionScreen({Key? key}) : super(key: key);
-
   @override
   _SignupOptionScreenState createState() => _SignupOptionScreenState();
 }
 
 class _SignupOptionScreenState extends State<SignupOptionScreen> {
+  var passwordNode = FocusNode();
+  var confirmPasswordNode = FocusNode();
   String? userEmail, userPassword, confirmPassword;
   final GlobalKey<FormState> _formKey = GlobalKey();
+  var emailController = TextEditingController();
+  var passController = TextEditingController();
+  var confirmPassController = TextEditingController();
+
   void _SignUp() async {
     if (!_formKey.currentState!.validate()) {
       return;
@@ -20,7 +24,11 @@ class _SignupOptionScreenState extends State<SignupOptionScreen> {
     _formKey.currentState!.save();
     if (userPassword == confirmPassword) {
       try {
-        await EmailAuth().signup(userEmail!, userPassword!);
+        await EmailAuth().signup(
+            email: userEmail!, password: userPassword!, context: context);
+        emailController.clear();
+        passController.clear();
+        confirmPassController.clear();
       } on Exception catch (error) {
         String error_message = '';
         if (error.toString().contains('EMAIL_EXISTS')) {
@@ -39,6 +47,16 @@ class _SignupOptionScreenState extends State<SignupOptionScreen> {
     } else {
       dialogBox("Password and ConfirmPassword is not same");
     }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passController.dispose();
+    confirmPassController.dispose();
+    passwordNode.dispose();
+    confirmPasswordNode.dispose();
+    super.dispose();
   }
 
   void dialogBox(String message) {
@@ -69,130 +87,150 @@ class _SignupOptionScreenState extends State<SignupOptionScreen> {
               VideoApp(
                 vidPath: 'assets/bv.mp4',
               ),
-              Form(
-                key: _formKey,
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      TextFormField(
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                        validator: (value) {
-                          if (value.toString().isEmpty) {
-                            return 'This field cannot be empty';
-                          }
-
-                          if (value!.isEmpty || !value.contains('@')) {
-                            return 'Invalid email!';
-                          }
-                          return null;
-                        },
-                        onSaved: (val) {
-                          userEmail = val!;
-                        },
-                        decoration: InputDecoration(
-                          hintStyle: const TextStyle(
-                              color: Colors.black54,
-                              fontWeight: FontWeight.bold),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(15)),
-                          hintText: 'Enter E-Mail',
-                        ),
-                        textInputAction: TextInputAction.next,
-                        onFieldSubmitted: (_) {},
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                        validator: (value) {
-                          if (value!.isEmpty || value.length < 5) {
-                            return 'Password is too short!';
-                          }
-                        },
-                        onSaved: (val) {
-                          userPassword = val!;
-                        },
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintStyle: const TextStyle(
-                              color: Colors.black54,
-                              fontWeight: FontWeight.bold),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(15)),
-                          hintText: 'Enter Password',
-                        ),
-                        textInputAction: TextInputAction.next,
-                        onFieldSubmitted: (_) {},
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        style: const TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                        validator: (value) {
-                          if (value.toString().isEmpty) {
-                            return 'This field cannot be empty';
-                          }
-                        },
-                        onSaved: (val) {
-                          confirmPassword = val!;
-                        },
-                        decoration: InputDecoration(
-                          hintStyle: const TextStyle(
-                              color: Colors.black54,
-                              fontWeight: FontWeight.bold),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(15)),
-                          hintText: 'Confirm Password',
-                        ),
-                        textInputAction: TextInputAction.next,
-                        onFieldSubmitted: (_) {},
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            _SignUp();
-                          },
-                          child: const Text(
-                            'Sign-Up',
-                            style: TextStyle(color: Colors.white),
+              Center(
+                child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
                           ),
-                          style: TextButton.styleFrom(
-                            minimumSize: const Size(double.infinity, 50),
-                            backgroundColor: Colors.indigo,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15)),
-                          ))
-                    ],
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          TextFormField(
+                            controller: emailController,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                            validator: (value) {
+                              if (value.toString().isEmpty) {
+                                return 'This field cannot be empty';
+                              }
+
+                              if (value!.isEmpty || !value.contains('@')) {
+                                return 'Invalid email!';
+                              }
+                              return null;
+                            },
+                            onSaved: (val) {
+                              userEmail = val!;
+                            },
+                            decoration: InputDecoration(
+                              hintStyle: const TextStyle(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.bold),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15)),
+                              hintText: 'Enter E-Mail',
+                            ),
+                            textInputAction: TextInputAction.next,
+                            onFieldSubmitted: (_) {
+                              FocusScope.of(context).requestFocus(passwordNode);
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: passController,
+                            focusNode: passwordNode,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                            validator: (value) {
+                              if (value!.isEmpty || value.length < 5) {
+                                return 'Password is too short!';
+                              }
+                            },
+                            onSaved: (val) {
+                              userPassword = val!;
+                            },
+                            onFieldSubmitted: (_) {
+                              FocusScope.of(context)
+                                  .requestFocus(confirmPasswordNode);
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintStyle: const TextStyle(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.bold),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15)),
+                              hintText: 'Enter Password',
+                            ),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: confirmPassController,
+                            focusNode: confirmPasswordNode,
+                            obscureText: true,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                            validator: (value) {
+                              if (value.toString().isEmpty) {
+                                return 'This field cannot be empty';
+                              }
+                            },
+                            onSaved: (val) {
+                              confirmPassword = val!;
+                            },
+                            decoration: InputDecoration(
+                              hintStyle: const TextStyle(
+                                  color: Colors.black54,
+                                  fontWeight: FontWeight.bold),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15)),
+                              hintText: 'Confirm Password',
+                            ),
+                            textInputAction: TextInputAction.next,
+                            onFieldSubmitted: (_) {},
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                _SignUp();
+                              },
+                              child: const Text(
+                                'Sign-Up',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              style: TextButton.styleFrom(
+                                minimumSize: const Size(double.infinity, 50),
+                                backgroundColor: Colors.indigo,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15)),
+                              )),
+                          const SizedBox(
+                            height: 2,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
